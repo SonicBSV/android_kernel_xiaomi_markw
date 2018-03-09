c9c2b98b4bc3812f0a9c52c9aa392858e4d4a38c
Anomalchik <vidslava@yandex.ru>
2018-03-09 18:05:25 +0300

    camera_v2: add option for disable IR_Led and IR_Cut driver it's need for TAG LA.UM.5.3 and lower

--- drivers/media/platform/msm/camera_v2/sensor/io/msm_camera_dt_util.c
+++ drivers/media/platform/msm/camera_v2/sensor/io/msm_camera_dt_util.c
@@ -285,6 +285,7 @@ int msm_sensor_get_sub_module_index(struct device_node *of_node,
 		src_node = NULL;
 	}
 
+#ifndef CONFIG_MSMB_WITHOUT_IR
 	src_node = of_parse_phandle(of_node, "qcom,ir-led-src", 0);
 	if (!src_node) {
 		CDBG("%s:%d src_node NULL\n", __func__, __LINE__);
@@ -316,6 +317,7 @@ int msm_sensor_get_sub_module_index(struct device_node *of_node,
 		of_node_put(src_node);
 		src_node = NULL;
 	}
+#endif
 
 	rc = of_property_read_u32(of_node, "qcom,strobe-flash-sd-index", &val);
 	if (rc != -EINVAL) {
@@ -876,6 +878,7 @@ int msm_camera_init_gpio_pin_tbl(struct device_node *of_node,
 		return rc;
 	}
 
+#ifndef CONFIG_MSMB_WITHOUT_IR
 	rc = of_property_read_u32(of_node, "qcom,gpio-ir-p", &val);
 	if (rc != -EINVAL) {
 		if (rc < 0) {
@@ -922,6 +925,7 @@ int msm_camera_init_gpio_pin_tbl(struct device_node *of_node,
 	} else {
 		rc = 0;
 	}
+#endif
 
 	rc = of_property_read_u32(of_node, "qcom,gpio-vana", &val);
 	if (rc != -EINVAL) {
