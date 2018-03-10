4fec11332e4dec99ed0a9387b4d5d3c7579a2ae4
Anomalchik <vidslava@yandex.ru>
2018-03-10 16:30:21 +0300

    markw: camera_v2: add markw changes (reverted from commit d26dde631e407657383fc5ae0eae5c9a113794b9)

--- drivers/media/platform/msm/camera_v2/sensor/csid/msm_csid.c
+++ drivers/media/platform/msm/camera_v2/sensor/csid/msm_csid.c
@@ -62,7 +62,7 @@
 #define FALSE  0
 
 #define MAX_LANE_COUNT 4
-#if defined CONFIG_MACH_XIAOMI_MIDO || CONFIG_MACH_XIAOMI_MARKW
+#ifdef CONFIG_MACH_XIAOMI_MIDO
 #define CSID_TIMEOUT msecs_to_jiffies(500)
 #else
 #define CSID_TIMEOUT msecs_to_jiffies(100)
